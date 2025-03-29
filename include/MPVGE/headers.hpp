437da3b0bd0a390130cfb3e2436074da8cc67de2
Clang Robot <robot@example.com>
2025-03-29 20:32:36 +0000

    :art: Applied clang-format to source code (auto-formatting) - Updated .h, .cpp, and .hpp files to conform with the style guide. - Changes include:   - Indentation adjustments   - Spacing improvements   - Alignment enhancements   - Consistency in formatting across files   - Removal of trailing whitespace

--- include/MPVGE/headers.hpp
+++ include/MPVGE/headers.hpp
@@ -1,8 +1,8 @@
 /*
-* Created by gbian on 29/03/2025.
-* Copyright (c) 2025 All rights reserved.
-*/
+ * Created by gbian on 29/03/2025.
+ * Copyright (c) 2025 All rights reserved.
+ */
 
 #pragma once
 
-#include  <mpvge_lib_core/mpvge_lib_core.hpp>
+#include <mpvge_lib_core/mpvge_lib_core.hpp>
