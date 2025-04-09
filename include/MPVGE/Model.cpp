ed565e752058cd4f1450940eceb3ac489a4f2477
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2025-04-09 14:48:29 +0200

    feat: add Model class and update CMake configuration
    - Added Model class in Model.hpp and Model.cpp to define the structure for model representation in the project.
    - Corrected the executable target name from 'mvpge' to 'mpvge' in CMakeLists.txt files across multiple directories to maintain consistency.
    - Included Model.cpp and Model.hpp in the mpvge_lib target to ensure proper compilation and linking of the new Model class.

---
+++ include/MPVGE/Model.cpp
@@ -0,0 +1,9 @@
+/*
+* Created by gbian on 09/04/2025.
+* Copyright (c) 2025 All rights reserved.
+*/
+
+#include "Model.hpp"
+
+namespace mpvge {
+} // mpvge
---
+++ include/MPVGE/Model.hpp
@@ -0,0 +1,14 @@
+/*
+* Created by gbian on 09/04/2025.
+* Copyright (c) 2025 All rights reserved.
+*/
+
+#pragma once
+
+namespace mpvge {
+
+class Model {
+
+};
+
+} // mpvge
